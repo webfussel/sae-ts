695d9de39f842bb5c2da829a578b866590605f21
webfussel <fiona@webfussel.de>
2023-05-17 13:27:21 +0200

    ADD: classes

--- src/02_TypeScript/06_window.ts
+++ src/02_TypeScript/06_window.ts
@@ -1,3 +1,4 @@
+// schaut euch die datei globals.d.ts an
 const replaceDOM = () => {
     document.documentElement.innerHTML = 'hi'
 }
---
+++ src/02_TypeScript/08_class.ts
@@ -0,0 +1,55 @@
+class Entity {
+    private readonly _name: string
+
+    constructor(name: string) {
+        this._name = name
+    }
+
+    get name(): string {
+        return this._name
+    }
+}
+
+class HealthEntity extends Entity {
+    private _health: number = 0
+
+    constructor(name: string, health: number) {
+        super(name)
+        this._health = health
+    }
+
+    gainHealth(health: number): void {
+        this._health += health
+    }
+
+    loseHealth(health: number): void {
+        this._health -= health
+    }
+
+    set health(value: number) {
+        this._health = value
+    }
+
+    get health(): number {
+        return this._health
+    }
+}
+
+function AddDebug<TBase extends new (...args: any[]) => {}> (Base : TBase) {
+    return class DebugEntity extends Base {
+        print(): void {
+            console.log(`THIS IS A DEBUG ENTITY`, (this as any)['name'])
+        }
+    }
+}
+
+const DebugNPC = AddDebug(HealthEntity)
+
+const player = new HealthEntity('Player', 100)
+const enemy = new DebugNPC('Enemy', 40)
+
+player.gainHealth(10)
+enemy.loseHealth(10)
+
+console.log('08: CLASS', player)
+console.log('08: CLASS', enemy)
